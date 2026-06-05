151e9acbcc86e889472fcfbf3b51aaeae064faec
brythany avila <brythanyavila14@gmail.com>
2026-06-05 15:25:29 -0600

    Testing Hooks, Custom Hooks, Asynchronous Tasks, Tasks Involving Timeouts, Axios, Integrating Testing into the Build Process Spies Writing Functions for Testing Exception Handling

--- src/counter/components/MyCounterApp.tsx
+++ src/counter/components/MyCounterApp.tsx
@@ -1,13 +1,13 @@
 import { useCounter } from "../hooks/useCounter";
 
 export const MyCounterApp = () => {
-  const { counter, handleAdd, handleSubtract, handleReset } = useCounter(5);
+  const { counter, handleAdd, handleSubtract, handleReset } = useCounter(10);
 
   return (
     <div
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
-      <h1>counter: {counter} </h1>
+      <h1>counter: {counter}</h1>
 
       <div style={{ display: "flex", gap: "10px" }}>
         <button onClick={handleAdd}>+1</button>
